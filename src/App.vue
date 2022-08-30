28cf4337ee3a81890823e35b01c0384fb2fdf2e6
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2022-08-30 22:42:02 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+
+    <div class="wrapper container">
+      <h1>สินค้าในร้าน</h1>
+      <button class="btn btn-success">เพิ่มสินค้า</button>
+      <main>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">ชื่อสินค้า</th>
+              <th scope="col">รูปภาพ</th>
+              <th scope="col">คำอธิบาย</th>
+              <th scope="col">ราคา</th>
+              <th scope="col">จำนวน</th>
+              <th scope="col">หมวดหมู่</th>
+              <th scope="col">แก้ไขข้อมูล</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td class="name" scope="row"></td>
+              <td class="pic">
+                <img />
+              </td>
+              <td class="price"></td>
+              <td class="edit">
+                <button class="edit btn btn-warning">
+
+                </button>
+                <button class="edit btn btn-outline-danger">
+                  ลบ
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
-    <router-view />
+
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueSweetalert2 from "vue-sweetalert2";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
+import "sweetalert2/dist/sweetalert2.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueFbCustomerChat from "vue-fb-customer-chat";
+
+Vue.use(VueFbCustomerChat, {
+  page_id: "527689331030224", //  change 'null' to your Facebook Page ID,
+  theme_color: "#333333", // theme color in HEX
+  locale: "en_US", // default 'en_US'
+});
+Vue.use(BootstrapVue);
+// Icons
+Vue.use(IconsPlugin);
+Vue.use(VueSweetalert2);
 Vue.config.productionTip = false;
 
 new Vue({
